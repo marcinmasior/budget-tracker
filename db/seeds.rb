67f54564fec1cac95ac781ad03baa3773b260769
Marcin Mąsior <marcinmasior@gmail.com>
2023-10-17 18:09:12 +0200

    Add devise and users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+if User.all.empty?
+  User.create!(
+    email: 'user@local',
+    password: 'password',
+    password_confirmation: 'password'
+  )
+  puts 'User has been created'
+end
